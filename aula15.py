ba563f336a0dfe8ecedaf117cf9608c333d30f94
tavaresdev-2210 <lucas.t.rosa@outlook.com>
2026-06-17 13:58:11 -0300

    Add files via upload

---
+++ aula15.py
@@ -0,0 +1,18 @@
+# #print(f'O seu nome é {nome}')
+#nome = input('Qual o seu nome?
+#Nesta aula iremos usar a funcao input para coletar dados do usuario 
+
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite outro número '))
+print(f'A soma dos numeros é: {numero_1 + numero_2}')
+
+#com o int na linha conseguimos no terminal fazer a soma
+#ja sem o int acaba formando uma concatenacao juntando um numero com outro 
+#quando usamos o str com o sinal de + acontece contatenação
+        
+
+
+
+
+
+
